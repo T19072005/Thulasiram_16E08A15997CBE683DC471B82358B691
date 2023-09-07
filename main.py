b6a38b67989f8b42fb471e832db4ae66b3fd6bfc
T19072005 <144225532+T19072005@users.noreply.github.com>
2023-09-07 11:00:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-def isleapYear(year):
-  if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    return True
-  else:
-    return False
+def factorial(n):
+  if n == 0:
+    return 1
+  return n * factorial(n - 1)
 
 
-year =int(input(" Enter a year :"))
-
-if isleapYear(year):
-  print('{} is a leap year. '.format(year))
-else:
-  print('{} is not a leap year. '.format(year))
+num = int(input("Enter the value:"))
+print("Factorial of", num, "is", factorial(num))
